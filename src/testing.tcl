d49dc8f6fc6f8278d0ab8c7554ff83b9733b4702
brigio345 <brigio345@gmail.com>
2020-06-08 21:14:52 +0200

    feat(testing): introduce test_dependencies
    Signed-off-by: brigio345 <brigio345@gmail.com>

---
+++ src/testing.tcl
@@ -0,0 +1,30 @@
+# test_dependencies:
+#	* argument(s):
+#		- start_time_lst: list of pairs <node_id, start_time>
+#		- fu_id_lst: list of pairs <node_id, fu_id>
+#	* return: 
+#		1 if DFG dependencies are satisfied by inputs, 0 otherwise.
+proc test_dependencies {start_time_lst fu_id_lst} {
+	for {set i 0} {$i < [llength $start_time_lst]} {incr i} {
+		set node_pair [lindex $start_time_lst $i]
+		set node [lindex $node_pair 0]
+		set node_start [lindex $node_pair 1]
+		set fu_i [lsearch -index 0 $fu_id_lst $node]
+		set node_fu [lindex [lindex $fu_id_lst $fu_i] 1]
+		set node_delay [get_attribute $node_fu delay]
+		set node_end [expr {$node_start + $node_delay}]
+		foreach child [get_attribute $node children] {
+			set child_i [lsearch -index 0 $start_time_lst $child]
+			set child_start [lindex $start_time_lst $child_i]
+
+			if {$child_start < $node_end} {
+				puts "Node $node completing at $node_end"
+				puts "Child $child scheduled at $child_start"
+				return 0
+			}
+		}
+	}
+
+	return 1
+}
+
