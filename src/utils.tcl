d9e6ff11b348830f4c72e84f57186ff098d82786
brigio345 <brigio345@gmail.com>
2020-06-12 20:56:16 +0200

    feat(utils.get_sorted_selected_fus): sort fus by power and area too
    Signed-off-by: brigio345 <brigio345@gmail.com>

--- src/utils.tcl
+++ src/utils.tcl
@@ -127,42 +127,64 @@ proc get_sorted_selected_fus_dict {} {
 
 		# avoid adding duplicates
 		if {[dict exists $fus_dict $op] == 0} {
-			# label fus with their delays (needed for sorting fus by delay)
-			set fu_delay_lst [list]
+			# label fus with their area, delay and power
+			# (needed for sorting)
+			set fu_specs_lst [list]
 			foreach op_fu [get_lib_fus_from_op $op] {
-				lappend fu_delay_lst "$op_fu [get_attribute $op_fu delay]"
+				set delay [get_attribute $op_fu delay]
+				set power [expr {[get_attribute $op_fu power] * $delay}]
+				set area [get_attribute $op_fu area]
+				lappend fu_specs_lst [list $op_fu $delay $power $area]
 			}
+			
+			# first sort by area and power and then by delay, in order
+			# to make sure to include only the most convenient fu for
+			# each delay value (e.g. same delay, but bigger area or
+			# power)
+			set fu_specs_by_area_lst [lsort -integer -index 3 $fu_specs_lst]
+			set fu_specs_by_power_lst [lsort -integer -index 2 $fu_specs_by_area_lst]
+			set fu_specs_sorted_lst [lsort -integer -index 1 $fu_specs_by_power_lst]
 
-			set fu_delay_sorted_lst [lsort -integer -index 1 $fu_delay_lst]
-
-			# always include fastest fu
-			set fu [lindex [lindex $fu_delay_sorted_lst 0] 0]
-			set area [get_attribute $fu area]
-			set min_delay [get_attribute $fu delay]
-			set power [expr {[get_attribute $fu power] * $min_delay}]
-
+			# always include fastest and most convenient fu
+			set fu_specs [lindex $fu_specs_sorted_lst 0]
+			set fu [lindex $fu_specs 0]
+			set delay [lindex $fu_specs 1]
+			set min_delay $delay
+			set power [lindex $fu_specs 2]
+			set area [lindex $fu_specs 3]
 			set op_fu_dict [dict create fu $fu delta 0]
 			set sorted_op_fus_lst [list $op_fu_dict]
 
 			# filter out non-convenient fus
-			foreach fu_delay $fu_delay_sorted_lst {
-				set fu [lindex $fu_delay 0]
-				
+			foreach fu_specs $fu_specs_sorted_lst {
+				set delay_prev $delay
+				set delay [lindex $fu_specs 1]
+
+				# since fus are sorted by delay, power and area
+				# the first fu with a certain delay is the best
+				# possible: if current fu has same delay as
+				# previous one, it is possible to furtherly,
+				# since it cannot provide any improvement
+				if {$delay == $delay_prev} {
+					continue
+				}
+
 				set area_prev $area
 				set power_prev $power
-				set area [get_attribute $fu area]
-				set delay [get_attribute $fu delay]
-				set power [expr {[get_attribute $fu power] * $delay}]
+
+				set power [lindex $fu_specs 2]
+				set area [lindex $fu_specs 3]
 				
 				# skip current fu if it leads to higher power or
 				# it leads to the same power and same or bigger
 				# area, with respect to previous fu (which has a
 				# lower or equal delay)
 				if {$power > $power_prev ||
-					($power == $power_prev && $area >= $area_prev)} {
+						($power == $power_prev && $area >= $area_prev)} {
 					continue
 				}
 
+				set fu [lindex $fu_specs 0]
 				set delta [expr $delay - $min_delay]
 
 				set op_fu_dict [dict create fu $fu delta $delta]
