edcd3adddb43840fd109e19548a73c1cef77e6b6
brigio345 <brigio345@gmail.com>
2020-07-05 13:56:45 +0200

    feat(testing): test multiple libraries and introduce print_info
    Signed-off-by: brigio345 <brigio345@gmail.com>

--- src/testing.tcl
+++ src/testing.tcl
@@ -6,70 +6,76 @@ source ./contest/src/brave_opt.tcl
 #	* return: 
 #		1 if all tests are passed, 0 otherwise.
 proc test_suite {} {
+	set libs_root "./data/RTL_libraries/"
 	set DFGs_root "./data/DFGs/"
 
 	# TODO: find a better way to set lambda
 	set lambda 80
 
 	set success 1
-	foreach dfg [glob $DFGs_root/*.dot] {
-		puts "Testing DFG $dfg"
-		read_design $dfg
+	foreach lib [glob $libs_root/*.txt] {
+		read_library $lib
+		foreach dfg [glob $DFGs_root/*.dot] {
+			puts "Testing DFG $dfg with library $lib"
+			read_design $dfg
 
-		puts "Executing algorithm..."
+			puts "Executing algorithm..."
 
-		set start_time [clock clicks -milliseconds]
+			set start_time [clock clicks -milliseconds]
 
-		set res [brave_opt -lambda $lambda]
+			set res [brave_opt -lambda $lambda]
 
-		set end_time [clock clicks -milliseconds]
+			set end_time [clock clicks -milliseconds]
 
 
-		set node_start_lst [lindex $res 0]
-		set node_fu_lst [lindex $res 1]
-		set fu_alloc_lst [lindex $res 2]
+			set node_start_lst [lindex $res 0]
+			set node_fu_lst [lindex $res 1]
+			set fu_alloc_lst [lindex $res 2]
 
-		puts -nonewline "Testing occurrencies..."
-		if {[test_occurrencies $node_start_lst $node_fu_lst $fu_alloc_lst] == 1} {
-			puts "\t\t\tOK"
-		} else {
-			puts "\t\t\tFAIL"
-			set success 0
+			puts -nonewline "Testing occurrencies..."
+			if {[test_occurrencies $node_start_lst $node_fu_lst $fu_alloc_lst] == 1} {
+				puts "\t\t\tOK"
+			} else {
+				puts "\t\t\tFAIL"
+				set success 0
+			}
+
+			puts -nonewline "Testing latency violations..."
+			if {[test_latency $node_start_lst $node_fu_lst $lambda] == 1} {
+				puts "\t\tOK"
+			} else {
+				puts "\t\tFAIL"
+				set success 0
+			}
+
+			puts -nonewline "Testing dependecies violations..."
+			if {[test_dependencies $node_start_lst $node_fu_lst] == 1} {
+				puts "\tOK"
+			} else {
+				puts "\tFAIL"
+				set success 0
+			}
+
+			puts -nonewline "Testing fus conflicts..."
+			if {[test_fu_conflicts $node_start_lst $node_fu_lst $fu_alloc_lst] == 1} {
+				puts "\t\tOK"
+			} else {
+				puts "\t\tFAIL"
+				set success 0
+			}
+
+			puts "INFO:"
+
+			puts "CPU execution time: \t\t\t[expr {$end_time - $start_time}] ms"
+			puts "Total power: \t\t\t\t[get_total_power [lindex $res 1]]"
+			puts "Total area: \t\t\t\t[get_total_area [lindex $res 2]]"
+
+			puts ""
+
+			remove_design
 		}
 
-		puts -nonewline "Testing latency violations..."
-		if {[test_latency $node_start_lst $node_fu_lst $lambda] == 1} {
-			puts "\t\tOK"
-		} else {
-			puts "\t\tFAIL"
-			set success 0
-		}
-
-		puts -nonewline "Testing dependecies violations..."
-		if {[test_dependencies $node_start_lst $node_fu_lst] == 1} {
-			puts "\tOK"
-		} else {
-			puts "\tFAIL"
-			set success 0
-		}
-
-		puts -nonewline "Testing fus conflicts..."
-		if {[test_fu_conflicts $node_start_lst $node_fu_lst $fu_alloc_lst] == 1} {
-			puts "\t\tOK"
-		} else {
-			puts "\t\tFAIL"
-			set success 0
-		}
-
-		puts "INFO:"
-
-		puts "CPU execution time: \t\t\t[expr {$end_time - $start_time}] ms"
-		puts "Total power: \t\t\t\t[get_total_power [lindex $res 1]]"
-		puts "Total area: \t\t\t\t[get_total_area [lindex $res 2]]"
-
-		puts ""
-
-		remove_design
+		remove_library
 	}
 
 	return $success
@@ -218,5 +224,27 @@ proc test_fu_conflicts {node_start_lst node_fu_lst fu_alloc_lst} {
 	}
 
 	return 1
+}
+
+proc print_info {node_start_lst node_fu_lst} {
+	set end_max 0
+	foreach node_start_pair $node_start_lst {
+		set node [lindex $node_start_pair 0]
+		set start [lindex $node_start_pair 1]
+		set node_fu_pair [lsearch -inline -index 0 $node_fu_lst $node]
+		set fu [lindex $node_fu_pair 1]
+
+		set delay [get_attribute $fu delay]
+
+		set end [expr {$start + $delay}]
+		
+		if {$end > $end_max} {
+			set end_max $end
+		}
+
+		puts "node=$node; fu=$fu; start=$start; end=$end"
+	}
+
+	puts "Latency=$end_max"
 }
 
